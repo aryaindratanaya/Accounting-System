7a8ea0c177f2715c1bc9ab277fd6b60410c1ef5a
Indra Tanaya <aryaindratanaya@gmail.com>
2021-01-13 23:06:43 +0800

    Create the structure of the project

---
+++ Code.js
@@ -0,0 +1,44 @@
+const ss = SpreadsheetApp.getActiveSpreadsheet();
+
+function onOpen() {
+  createMenu();
+}
+
+function createMenu() {
+  const ui = SpreadsheetApp.getUi();
+  const menu = ui.createMenu("Start");
+  menu.addItem("Cashier", "loadCashier");
+  menu.addToUi();
+}
+
+// Functions below display the pages
+function loadCashier() {
+  const htmlServ = HtmlService.createTemplateFromFile("Cashier");
+  const html = htmlServ.evaluate().setWidth(1000).setHeight(550);
+  const ui = SpreadsheetApp.getUi();
+  ui.showModelessDialog(html, "Cashier");
+}
+
+// Repeated operations
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
+
+function arrayValidator(anyArray) {
+  anyArray.map((r) => r[0]);
+  return anyArray;
+}
+
+function getLastRowColumn(range) {
+  var rowNum = 0;
+  var blank = false;
+  for (var row = 0; row < range.length; row++) {
+    if (range[row][0] === "" && !blank) {
+      rowNum = row;
+      blank = true;
+    } else if (range[row][0] !== "") {
+      blank = false;
+    }
+  }
+  return rowNum;
+}
