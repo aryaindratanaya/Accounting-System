bcb0df762418323ad5a4b3ecefaec7ced91eb96c
Indra Tanaya <aryaindratanaya@gmail.com>
2021-01-22 18:52:29 +0800

    Enable writing data to sheet sales

--- Code.js
+++ Code.js
@@ -15,46 +15,66 @@ function createMenu() {
 function loadCashier() {
   const htmlServ = HtmlService.createTemplateFromFile("Cashier");
   htmlServ.drinks = getDrinks();
+  htmlServ.foods = getFoods();
   const html = htmlServ.evaluate().setWidth(1000).setHeight(1000);
   const ui = SpreadsheetApp.getUi();
   ui.showModelessDialog(html, "Cashier");
 }
 
 // Load entries from sheet
+function getOrderId() {
+  const ws = ss.getSheetByName("Sales");
+  const entries = ws
+    .getRange(getLastRowColumn(ws.getRange("B:B").getValues()), 2, 1, 2)
+    .getValues();
+  return arrayValidator(entries);
+}
+
+function getFoods() {
+  const ws = ss.getSheetByName("Menu");
+  const entries = ws
+    .getRange(4, 3, getLastRowColumn(ws.getRange("C:C").getValues()) - 3, 2)
+    .getValues();
+  return arrayValidator(entries);
+}
+
 function getDrinks() {
   const ws = ss.getSheetByName("Menu");
   const entries = ws
-    .getRange(4, 7, getLastRowColumn(ws.getRange("F:F").getValues()) - 3, 2)
+    .getRange(4, 7, getLastRowColumn(ws.getRange("G:G").getValues()) - 3, 2)
     .getValues();
   return arrayValidator(entries);
 }
 
 // Add entries to sheet
-function addSales(id, sltEatOptions, orders) {
-  const now = new Date();
+function addSales(id, sltEatOptions, orders, date, time) {
   let salesEntry = [];
+  const curOrderId = isNaN(getOrderId()[0][0]) ? 1 : getOrderId()[0][0] + 1;
+  // const curRow = getLastRowColumn(ws.getRange("B:B").getValues()) + 1;
 
   orders.forEach((order, i) => {
-    const currentFnB = Object.keys(order)[0];
+    const curFnB = Object.keys(order)[0];
     salesEntry.push([
+      curOrderId + i,
       id,
-      currentFnB,
-      orders[i][currentFnB].quantity,
-      orders[i][currentFnB].price,
+      curFnB,
+      orders[i][curFnB].quantity,
+      orders[i][curFnB].price,
       sltEatOptions,
-      orders[i][currentFnB].notes,
-      now.getDate() + "-" + now.getMonth() + "-" + now.getFullYear(),
-      now.getTime(),
+      orders[i][curFnB].notes,
+      date,
+      time,
     ]);
   });
 
   const ws = ss.getSheetByName("Sales");
   ws.getRange(
     getLastRowColumn(ws.getRange("C:C").getValues()) + 1,
-    3,
+    2,
     salesEntry.length,
-    8
+    9
   ).setValues([...salesEntry]);
+  // setBackgroundColor("Sales", curRow, 2, salesEntry.length, 9);
 }
 
 // Repeated operations
@@ -79,4 +99,9 @@ function getLastRowColumn(range) {
     }
   }
   return rowNum;
+}
+
+function setBackgroundColor(sheet, row1, col1, row2, col2) {
+  const range = ss.getSheetByName(sheet).getRange(row1, col1, row2, col2);
+  range.setBackgroundColor("grey");
 }
